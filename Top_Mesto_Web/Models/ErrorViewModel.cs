0afcafcb0c142640f215e0aece283f1f8202ea19
sntc4 <Egor.Borisov@unilever.com>
2020-02-23 00:13:08 +0600

    Добавьте файлы проекта.

---
+++ Top_Mesto_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Top_Mesto_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
